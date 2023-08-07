b9c4509f6c825a4d1db0098c335cc19c026cc3b2
rehannriaz <u2021438@giki.edu.pk>
2023-08-07 14:38:25 +0500

    .

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import {Menu, icons} from 'lucide-react'
 import {Button} from '@/components/ui/button'
-const Navbar = ( ) => {
+const   Navbar = ( ) => {
     return (
         <div className="flex items-center">
             <Button variant={'ghost'} size={'icon'} className='md:hidden'>
